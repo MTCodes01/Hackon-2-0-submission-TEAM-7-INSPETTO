6cace91773c572e4efa0f501986d7fca96824c35
MTCodes01 <sreedevss05@gmail.com>
2025-07-13 12:08:56 +0530

    updated start.bat

--- start.bat
+++ start.bat
@@ -1,13 +1,11 @@
 @echo off
 
-REM Start Cloudflare tunnel
-start cmd /k "cloudflared tunnel --config C:\Users\sreed\.cloudflared\config.yml run"
-
 REM Move to simulation directory
 cd RSSI Testing
 
 REM Start 2 python files
 start cmd /k "python beacon-simulation.py"
+timeout /t 1
 start cmd /k "python hub-simulation.py"
 
 REM Move to backend directory
@@ -16,5 +14,9 @@ cd backend
 
 REM Start Django server
 start cmd /k "python manage.py runserver"
+timeout /t 1
+
+REM Start Cloudflare tunnel
+start cmd /k "cloudflared tunnel --config C:\Users\sreed\.cloudflared\config.yml run"
 
 start chrome https://inspetto.ceal.in
